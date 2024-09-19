27c3fc74c8e877a5b616902aa1655e5da134bbdf
Mark Junker <mark.junker@dataline.de>
2024-09-19 22:33:57 +0200

    Projektdateien hinzufügen.

---
+++ TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+
+namespace TestReactiveUiSourceGenBug2;
+
+internal partial class TestViewModel : ReactiveObject
+{
+    [Reactive]
+    private string? _test;
+}
